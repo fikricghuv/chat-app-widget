24efb2c3331f254c6a0b3e4480d27857ef04afa4
M Fikri A <fikricghuv@gmail.com>
2025-09-03 08:52:52 +0700

    delete nginx config file, update const environment talkvera

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,11 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiKey: 'TEST API KEY',
-    backendApiUrl: 'http://brins.localhost:8001', 
-    websocketUrl: 'ws://brins.localhost:8001/ws/chat',
+    // apiKey: 'TEST API KEY',
+    // backendApiUrl: 'http://brins.localhost:8001', 
+    // websocketUrl: 'ws://brins.localhost:8001/ws/chat',
+
+    apiKey: 'TALKVERA API KEY',
+    backendApiUrl: 'http://talkvera.localhost:8001', 
+    websocketUrl: 'ws://talkvera.localhost:8001/ws/chat',
   };
